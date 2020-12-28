9fe74c064929b35229c6fdb38c0c1741d68cfbc9
shankarpriyank <shankarpriyank@312002.gmail.com>
2020-12-28 23:00:58 +0530

    First Commit

--- app/src/main/java/com/example/memeshareapp/MainActivity.kt
+++ app/src/main/java/com/example/memeshareapp/MainActivity.kt
@@ -1,42 +1,34 @@
 package com.example.memeshareapp
 
 
-import android.Manifest
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.graphics.drawable.Drawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
-import android.widget.TextView
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
-import com.android.volley.Request.Method.GET
-
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+
 var requestcode =1
 var imageurl:String? = null
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        Toast.makeText(this,"Swipe to Change Current image",Toast.LENGTH_LONG).show()
 
+        val snackbar = Snackbar.make(findViewById(android.R.id.content), "Swipe to Change The Memes", Snackbar.LENGTH_LONG)
+        snackbar.show()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -50,17 +42,17 @@ class MainActivity : AppCompatActivity() {
 
             override fun onSwipeBottom() {
                 super.onSwipeBottom()
-              loadmeme()
+                loadmeme()
             }
 
             override fun onSwipeLeft() {
                 super.onSwipeLeft()
-             loadmeme()
+                loadmeme()
             }
 
             override fun onSwipeRight() {
                 super.onSwipeRight()
-               loadmeme()
+                loadmeme()
             }
         })
     }
@@ -75,21 +67,21 @@ class MainActivity : AppCompatActivity() {
         val url = "https://meme-api.herokuapp.com/gimme"
 
 // Request a string response from the provided URL.
-        val jsonObjectRequest =JsonObjectRequest(Request.Method.GET,url,null,Response.Listener{ response ->
+        val jsonObjectRequest =JsonObjectRequest(Request.Method.GET, url, null, Response.Listener { response ->
             imageurl = response.getString("url")
 
 
-        Glide.with(this).load(imageurl).listener(object: RequestListener<Drawable>{
-            override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
-                return false
-            }
+            Glide.with(this).load(imageurl).listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                    return false
+                }
 
-            override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
-                progress_circular.visibility=View.GONE
-                return false
-            }
+                override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                    progress_circular.visibility = View.GONE
+                    return false
+                }
 
-        }).into(imageView)
+            }).into(imageView)
         }, {
 
         })
@@ -169,8 +161,8 @@ class MainActivity : AppCompatActivity() {
     fun sharememe(view: View) {
         val intent  = Intent(Intent.ACTION_SEND)
         intent.type="text/plain"
-        intent.putExtra(Intent.EXTRA_TEXT,"Hey Check This Cool Meme $imageurl")
-        val chooser = Intent.createChooser(intent,"Share This Meme using")
+        intent.putExtra(Intent.EXTRA_TEXT, "Hey Check This Cool Meme $imageurl")
+        val chooser = Intent.createChooser(intent, "Share This Meme using")
         startActivity(chooser)
 
     }
